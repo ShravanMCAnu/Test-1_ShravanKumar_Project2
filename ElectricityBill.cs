34ed769c488c36d38abb38f099d76450c3051253
ShravanMCAnu <bskmc850@gmail.com>
2022-11-05 16:20:19 +0530

    Add project files.

---
+++ ElectricityBill.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_ShravanKumar_Project2
+{
+    public class ElectricityBill 
+    {
+        public string CustomerName { get; set; }
+        public string MeterNumber { get; set; }
+
+        double BillAmount;
+        public void CalculateElectricityBill(int _units, string _serviceType)
+        {
+            if (_serviceType == "d" ||_serviceType=="D")
+            {
+
+
+                if (_units < 100)
+                {
+                    BillAmount = _units * 0.35;
+                }
+                else if (_units < 200)
+                {
+                    BillAmount = _units * 0.85;
+                }
+                else if (_units < 400)
+                {
+                    BillAmount = _units * 1.15;
+                }
+                else if (_units < 700)
+                {
+                    BillAmount = _units * 2.05;
+                }
+                else
+                {
+                    BillAmount = _units * 2.80;
+                }
+            }
+            else
+            {
+                if (_units < 100)
+                {
+                    BillAmount = _units * 0.90;
+                }
+                else if (_units < 200)
+                {
+                    BillAmount = _units * 1.35;
+                }
+                else if (_units < 400)
+                {
+                    BillAmount = _units * 1.95;
+                }
+                else if (_units < 700)
+                {
+                    BillAmount = _units * 2.45;
+                }
+                else
+                {
+                    BillAmount = _units * 3.25;
+                }
+            }
+            Console.WriteLine("--ELECTRICITY BILL DETAILS--");
+            Console.WriteLine("Customer Name: " + CustomerName);
+            Console.WriteLine("Meter Number : " + MeterNumber);
+            Console.WriteLine("Electricity Bill  is: "+BillAmount.ToString("0.00"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test1_ShravanKumar_Project2;
+
+ElectricityBill objElectricityBill=new ElectricityBill();
+Console.WriteLine("Enter Customer Name");
+objElectricityBill.CustomerName = Console.ReadLine();
+Console.WriteLine("Enter Meter Number like., eg: TS14361");
+objElectricityBill.MeterNumber = Console.ReadLine();
+Console.WriteLine("Enter Number of units");
+int units = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Service Type, For Domestic 'd' or For Commercial 'c' ");
+string serviceType = Console.ReadLine();
+objElectricityBill.CalculateElectricityBill(units, serviceType);
